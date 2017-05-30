6cf8ad0124947aa3ad00135afd5fb91bc85fc68c
Sung J Byun <sungjbyun@D-173-250-161-118.dhcp4.washington.edu>
2017-05-29 17:00:48 -0700

    working on Components

---
+++ src/Components/Places/Places.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+export class Places extends Component  {
+  render() {
+    return (
+      <div>This is Places component</div>
+
+    )
+  }
+}
